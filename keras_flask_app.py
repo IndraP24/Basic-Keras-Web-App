334263a13a24095c11d3e3139bc3be6ff95153c4
IndraP24 <indrashis985@gmail.com>
2021-06-22 21:52:32 +0530

    Update imports from tensorflow

--- keras_flask_app.py
+++ keras_flask_app.py
@@ -1,8 +1,7 @@
 # import the necessary packages
-import tensorflow as tf
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications import imagenet_utils
 from PIL import Image
 import numpy as np
 from flask import Flask, request, jsonify
